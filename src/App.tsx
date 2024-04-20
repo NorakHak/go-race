7e61d2b0e23a67a91f502be22b050b632f4e00aa
Norak <norak.hak98@gmail.com>
2024-04-20 18:17:08 +0400

    Added react router for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Garage } from './components/Garage/Garage';
+import Winners from './components/Winners/Winners';
 
 function App() {
-  const [showGarage, setShowGarage] = useState<boolean>(true);
-
-  const showWinnersBlock = () => {
-    setShowGarage(false);
-  };
-
-  const showGarageBlock = () => {
-    setShowGarage(true);
-  };
-
   return (
     <div className='App'>
-      <Header
-        showWinnersBlock={showWinnersBlock}
-        showGarageBlock={showGarageBlock}
-      />
-      <main>{showGarage ? <Garage /> : <div>Winners</div>}</main>
+      <BrowserRouter basename='/'>
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<Garage />} />
+            <Route path='/winners' element={<Winners />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,22 @@
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 // @ts-ignore
 import arrow from '../../imgs/icons/arrows_right1.png';
 // @ts-ignore
 import logo from '../../imgs/img/logo.png';
 
-interface HeaderProps {
-  showWinnersBlock: () => void;
-  showGarageBlock: () => void;
-}
-
-export const Header: React.FC<HeaderProps> = ({
-  showWinnersBlock,
-  showGarageBlock,
-}) => {
+export const Header = () => {
   return (
     <header className='header'>
       <div className='header_container'>
         <div className='header_buttons'>
-          <button onClick={showGarageBlock} className='header_btn garage'>
-            Garage
-          </button>
-          <button onClick={showWinnersBlock} className='header_btn winners'>
-            Winners
-          </button>
+          <Link to='/'>
+            <button className='header_btn garage'>Garage</button>
+          </Link>
+          <Link to='/winners'>
+            <button className='header_btn winners'>Winners</button>
+          </Link>
         </div>
         <div className='header_logo_container'>
           <div className='arrow_wrapper'>
